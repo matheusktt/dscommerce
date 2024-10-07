dc27670f760949c7a3ecaa31cf6a06cecba25cfd
matheusktt <matheus_0119@outlook.com>
2024-10-06 23:53:54 -0300

    insert products

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -31,4 +31,17 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(product -> new ProductDTO(product));
     }
+
+    @Transactional()
+    public ProductDTO insert(ProductDTO dto){
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
 }
