e4bf8062c724b7f5f43f2b72a877fd65515b06be
matheusktt <matheus_0119@outlook.com>
2024-10-09 01:07:58 -0300

    creating database exception

---
+++ src/main/java/com/devsuperior/dscommerce/services/exceptions/DatabaseExeception.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscommerce.services.exceptions;
+
+public class DatabaseExeception extends RuntimeException {
+
+    public DatabaseExeception(String msg) {
+        super(msg);
+    }
+}
