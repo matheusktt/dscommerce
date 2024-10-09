ea93833360eec62401c673edd17a5543fd328a14
matheusktt <matheus_0119@outlook.com>
2024-10-09 01:29:48 -0300

    add validation constraints

--- src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
@@ -1,12 +1,21 @@
 package com.devsuperior.dscommerce.DTO;
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa ter no mínimo caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "o preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
