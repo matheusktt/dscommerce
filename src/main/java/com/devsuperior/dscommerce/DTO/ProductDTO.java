7fe22b32fb6fff0ca0f6040c8a26b6042bb69324
matheusktt <matheus_0119@outlook.com>
2025-09-03 12:02:24 -0300

    productDTO with categories

--- src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
@@ -1,27 +1,30 @@
 package com.devsuperior.dscommerce.DTO;
 
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import jakarta.validation.constraints.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDTO {
 
     private Long id;
-
     @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
     @NotBlank(message = "Campo requerido")
     private String name;
 
-    @Size(min = 10, message = "Descrição precisa ter no mínimo caracteres")
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
 
-    @Positive(message = "o preço deve ser positivo")
+    @NotNull(message = "Campo requerido")
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
-    public ProductDTO() {
-
-    }
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -31,51 +34,38 @@ public class ProductDTO {
         this.imgUrl = imgUrl;
     }
 
-    public ProductDTO(Product entity){
-        this.id = entity.getId();
-        this.name = entity.getName();
-        this.description = entity.getDescription();
-        this.price = entity.getPrice();
-        this.imgUrl = entity.getImgUrl();
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+        for (Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat));
+        }
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public Double getPrice() {
         return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
     }
 
     public String getImgUrl() {
         return imgUrl;
     }
 
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
 }
