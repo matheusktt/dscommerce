0ec8afe4755a4b1a8d1b0f2c109fd4824f558347
matheusktt <matheus_0119@outlook.com>
2025-09-03 22:06:35 -0300

    get categories

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.DTO.CategoryDTO;
+import com.devsuperior.dscommerce.DTO.OrderDTO;
+import com.devsuperior.dscommerce.services.CategoryService;
+import com.devsuperior.dscommerce.services.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
---
+++ src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dscommerce.repositories;
+
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
